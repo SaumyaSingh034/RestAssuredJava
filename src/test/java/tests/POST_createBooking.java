0f7251d40e52ff14f69c4ed78cc71f2f34772a70
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-17 19:06:11 +0530

    Create User Completed

--- src/test/java/tests/POST_createBooking.java
+++ src/test/java/tests/POST_createBooking.java
@@ -1,18 +1,25 @@
 package tests;
 
+import base.BaseSetUp;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import requestBuilder.CreateUserBuilder;
 import requests.CreateUser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
-public class POST_createBooking {
+public class POST_createBooking extends BaseSetUp {
     private CreateUser newUser;
     private CreateUser partialUser;
-    private int userId;
+    private String userId;
     private String token;
 
     @BeforeTest
@@ -24,15 +31,20 @@ public class POST_createBooking {
 
     @Test
     public void createBrandNewUser(){
-        userId = given().body(newUser)
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name",newUser.getName());
+        jsonAsMap.put("job",newUser.getJob());
+        userId = given().contentType(ContentType.JSON).
+                body("{\"name\":\""+newUser.getName()+"\",\"job\":\""+newUser.getJob()+"\"}")
+        //body(jsonAsMap)
                 .when()
                 .post("/api/users")
                 .then()
-                .statusCode(200)
+                .statusCode(201)
                 .assertThat()
                 .body("name",equalTo(newUser.getName()))
                 .body("job",equalTo("Astronaut"))
-                .body("id",notNullValue())
+               .body("id",notNullValue())
                 .body("createdAt",notNullValue())
                 .extract()
                 .path("id");
