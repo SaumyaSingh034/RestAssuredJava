b69fea9c28828d1a278ca18c433b503b009dfe70
Saumya Singh <singhsaumyait034@gmail.com>
2024-08-17 21:00:52 +0530

    Get Users

--- src/test/java/tests/GET_ReqresTest.java
+++ src/test/java/tests/GET_ReqresTest.java
@@ -2,11 +2,26 @@ package tests;
 
 import base.BaseSetUp;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class GET_ReqresTest extends BaseSetUp {
 
-    @BeforeTest
-    public void testSetUp(){
+  @Test
+  public void getListOfUsers(){
+      ArrayList<String> data = given().get("/api/users?page=2").then().statusCode(200).assertThat()
+              .extract().path("data");
+      System.out.println(data);
+  }
 
-    }
+  @Test
+  public void getSpecificUser(){
+      Map<String, Object> d = given().get("/api/users/2").then().statusCode(200)
+              .extract().path("data");
+      System.out.println(d);
+  }
 }
